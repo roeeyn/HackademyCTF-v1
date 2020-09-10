7863095da4ff954e2d4ac24daed9109922e1af28
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-09-09 19:03:38 -0500

    Added ostras challenge

---
+++ Challenges/Pwnable/1.Ostras/ostras.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_flag() {
+  printf("mblue{b13nv3n1d0_4l_4n4l1s1s_d3_b1n4r10s}");
+}
+
+int main() {
+  printf("Dicen que las mejores ostras son de Maviri\n");
+  printf("o era de Mazatlan?\n");
+  printf("La verdad no recuerdo, ni me gustan las ostras.\n");
+  return 0;
+}
